9c5bf66951e3740c0846d6215265fbfc1fbd8aac
Bruno Silva <bruno.silva@ada.ac.uk>
2023-11-24 09:19:36 +0000

    Fully functioning auth, final schema, working route

--- src/config/controller.ts
+++ src/config/controller.ts
@@ -7,6 +7,10 @@ import {
   ServiceErrorCodes,
   ServiceSuccessCodes,
 } from "./types";
+import verifyJwt from "src/utils/verify-jwt";
+import db from "data";
+import { User, user } from "data/schema";
+import { eq } from "drizzle-orm";
 
 export const successResponse = <T>({
   ctx,
@@ -26,20 +30,20 @@ export const successResponse = <T>({
 };
 
 export const errorResponse = async <E>({
-  res,
+  ctx,
   error,
   code = 400,
   data = error,
 }: {
-  res: Context;
+  ctx: Context;
   error?: string;
   code?: ServiceErrorCodes;
   data?: string | E;
 }) => {
   if (process.env[AppEnvs.ENV] !== Environments.PROD) {
-    res.status(code);
+    ctx.status(code);
 
-    return res.json({
+    return ctx.json({
       meta: {
         code,
         status: "error",
@@ -50,9 +54,9 @@ export const errorResponse = async <E>({
     });
   }
 
-  res.status(code);
+  ctx.status(code);
 
-  return res.json({
+  return ctx.json({
     meta: { code, status: "error", data },
   });
 };
@@ -65,12 +69,90 @@ interface ControllerProps {
   action: ActionFn;
 }
 const Controller =
-  ({}: ControllerProps) =>
+  ({ action }: ControllerProps) =>
   async (ctx: Context) => {
+    const repo = db();
+
+    const authHeader = ctx.req.raw.headers.get("Authorization");
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return errorResponse({
+        ctx,
+        data: "Invalid credentials",
+        code: 401,
+      });
+    }
+
+    const bearer = authHeader.split("Bearer ")[1];
+
+    const token = await verifyJwt(bearer);
+
+    if (token.isError) {
+      return errorResponse({
+        ctx,
+        data: "Invalid credentials",
+        code: 401,
+      });
+    }
+    const authId = token.data.sub;
+
+    if (!authId) {
+      return errorResponse({
+        ctx,
+        data: "Invalid credentials",
+        code: 401,
+      });
+    }
+
+    console.log("Auth ID: ", authId);
+
+    let dbUser: User | null = null;
+
+    const existingUser = await repo
+      .select()
+      .from(user)
+      .where(eq(user.authId, authId));
+
+    if (!existingUser.length) {
+      const createdAt = new Date();
+      const updatedAt = new Date();
+
+      const newUser = await repo.insert(user).values({
+        authId,
+        createdAt,
+        updatedAt,
+      });
+
+      dbUser = {
+        id: newUser[0].insertId,
+        name: null,
+        authId,
+        createdAt,
+        updatedAt,
+      };
+    } else {
+      dbUser = existingUser[0];
+    }
+
+    const response = await action({
+      body: ctx.body,
+      params: ctx.req.param,
+      query: ctx.req.query,
+      authId,
+      user: dbUser,
+    });
+
+    if (response.isError) {
+      return errorResponse({
+        ctx,
+        data: "Unknown error",
+      });
+    }
+
     return successResponse({
       ctx,
-      data: "test",
-      code: 200,
+      data: response.data,
+      code: response.code,
     });
   };
 
--- src/routes/task/list-tasks/index.ts
+++ src/routes/task/list-tasks/index.ts
@@ -1,9 +1,73 @@
 import { ActionProps, ActionResponse } from "config/types";
+import db from "data";
+import { task, tag, taskToTag, user, Task, Tag } from "data/schema";
+import { eq } from "drizzle-orm";
 
-const ListTasks = async ({}: ActionProps): ActionResponse<{}> => {
+interface TasksWithTags extends Omit<Task, "userId"> {
+  tags: ListTag[];
+}
+
+interface ListTag extends Omit<Tag, "updatedAt"> {}
+
+const ListTasks = async ({ authId }: ActionProps): ActionResponse<{}> => {
+  if (!authId) {
+    return {
+      isError: true,
+      code: 401,
+      data: "Unauthorised",
+    };
+  }
+
+  const repo = db();
+
+  const results = await repo
+    .select({
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      done: task.done,
+      dueDate: task.dueDate,
+      priority: task.priority,
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+      tag: {
+        id: tag.id,
+        name: tag.name,
+        background: tag.background,
+        createdAt: tag.createdAt,
+      },
+    })
+    .from(task)
+    .leftJoin(taskToTag, eq(taskToTag.taskId, task.id))
+    .leftJoin(tag, eq(tag.id, taskToTag.tagId))
+    .innerJoin(user, eq(user.authId, authId));
+
+  const data = results.reduce((acc, cur) => {
+    const taskId = cur.id;
+    const task =
+      acc[taskId] ||
+      (acc[taskId] = {
+        id: cur.id,
+        title: cur.title,
+        description: cur.description,
+        done: cur.done,
+        dueDate: cur.dueDate,
+        priority: cur.priority,
+        createdAt: cur.createdAt,
+        updatedAt: cur.updatedAt,
+        tags: [],
+      });
+
+    if (cur.tag) {
+      task.tags.push(cur.tag);
+    }
+
+    return acc;
+  }, {});
+
   return {
     isError: false,
-    data: {},
+    data: Object.values(data),
     code: 200,
   };
 };
