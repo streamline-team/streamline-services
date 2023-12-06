28af3909a61a0f2b32ea8e3651e25a3750378d16
Bruno Silva <bruno.silva@ada.ac.uk>
2023-12-06 22:05:54 +0000

    Cleanup deploy script

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,24 +2,13 @@ PACKAGE="streamline-service"
 
 docker build -f docker/service/Dockerfile --platform linux/amd64 -t $PACKAGE .
 
-gcloud auth print-access-token \
-    --impersonate-service-account services-deployer@$GCLOUD_PROJECT.iam.gserviceaccount.com | docker login \
+gcloud auth print-access-token | docker login \
     -u oauth2accesstoken \
     --password-stdin https://europe-west2-docker.pkg.dev
-
-# Shouldn't be needed:
-# cat .gcloud-service.json | docker login -u _json_key --password-stdin https://europe-west2-docker.pkg.dev
 
 docker tag $PACKAGE europe-west2-docker.pkg.dev/$GCLOUD_PROJECT/cloud-run/$PACKAGE
 
 docker push europe-west2-docker.pkg.dev/$GCLOUD_PROJECT/cloud-run/$PACKAGE
-
-# CLOUDSDK_CORE_DISABLE_PROMPTS=1 ./google-cloud-sdk/install.sh --usage-reporting=false --quiet
-
-# Shouldn't need this with identty provider
-# ./google-cloud-sdk/bin/gcloud auth activate-service-account services-deployer@$GCLOUD_PROJECT.iam.gserviceaccount.com \
-#     --key-file=.gcloud-service.json \
-#     --project=$GCLOUD_PROJECT
 
 gcloud run deploy $PACKAGE \
     --quiet \
