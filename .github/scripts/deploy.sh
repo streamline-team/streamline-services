9d74b224808bf3940a16ea175ab565b5e80a0c9e
Bruno Silva <54673205+brunogrcsada@users.noreply.github.com>
2023-12-07 00:18:50 +0000

    Add deploy back after google incident (#12)

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,6 +1,6 @@
 PACKAGE="streamline-service"
 
-docker build -f docker/service/Dockerfile -t $PACKAGE .
+docker build -f docker/service/Dockerfile --platform linux/amd64 -t $PACKAGE .
 
 gcloud auth print-access-token | docker login \
     -u oauth2accesstoken \
@@ -17,6 +17,7 @@ gcloud run deploy $PACKAGE \
     --vpc-connector=projects/$GCLOUD_PROJECT/locations/europe-west2/connectors/vpc-connector-01 \
     --vpc-egress=all-traffic \
     --service-account=cloud-run@$GCLOUD_PROJECT.iam.gserviceaccount.com \
+    --add-cloudsql-instances=$GCLOUD_PROJECT:europe-west2:streamline  \
     --region europe-west2 \
     --min-instances 0 \
-    --update-env-vars "DB_USER=$DB_USER,DB_HOST=$DB_HOST,DB_PASSWORD=u=RmnDZc46aQ3E9,APP_ENV=prod"
+    --update-env-vars "DB_USER=$DB_USER,DB_HOST=$DB_HOST,DB_PASSWORD=$DB_PASSWORD,APP_ENV=prod"
